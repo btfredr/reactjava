9a65bed41526b953e4d25ca1c0a1afbd1841f271
Fredrik Fordelsen <fredrikf2000@gmail.com>
2025-02-08 21:53:10 +0100

    added maven spring boot

---
+++ backend/src/main/java/fredrikfordelsen/nextjava/NextjavaApplication.java
@@ -0,0 +1,13 @@
+package fredrikfordelsen.nextjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextjavaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/fredrikfordelsen/nextjava/NextjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package fredrikfordelsen.nextjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
