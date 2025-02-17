88b4ec4d7d04fa7160f6bc1730bfa31afc7f8c1d
Fredrik Fordelsen <fredrikf2000@gmail.com>
2025-02-17 10:38:24 +0100

    set up states and useeffect for fetching from backend

--- frontend/src/app/components/EmployeeList.js
+++ frontend/src/app/components/EmployeeList.js
@@ -2,6 +2,19 @@ import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 
 const EmployeeList = () => {
+    const [employees, setEmployees] = useState([]);
+
+    useEffect(() => {
+        // Fetching employees from backend
+        axios.get("http://localhost:8081/api/employees")
+            .then((response) => {
+                setEmployees(response.data);
+            })
+            .catch((error) => {
+                console.error("There was an error while fetching employees.", error);
+            });
+    }, []);
+    
   return (
     <div>EmployeeList</div>
   )
