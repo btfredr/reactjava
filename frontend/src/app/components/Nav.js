1a26d825552bf9135ec1dd114158cba65131d72d
Fredrik Fordelsen <fredrikf2000@gmail.com>
2025-02-17 10:10:42 +0100

    fixed error with nav links

--- frontend/src/app/components/Nav.js
+++ frontend/src/app/components/Nav.js
@@ -6,19 +6,13 @@ const Nav = () => {
         <div className="nav">
             <ul className="nav-list">
                 <li className="nav-item">
-                    <Link href="/">
-                        <a>Hjem</a>
-                    </Link>
+                    <Link href="/">Hjem</Link>
                 </li>
                 <li className="nav-item">
-                    <Link href="/">
-                        <a>Om Oss</a>
-                    </Link>
+                <Link href="/">Om Oss</Link>
                 </li>
                 <li className="nav-item">
-                    <Link href="/">
-                        <a>Kontakt</a>
-                    </Link>
+                <Link href="/">Kontakt</Link>
                 </li>
             </ul>
         </div>
