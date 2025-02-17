6cb657d4691af258bfc8c8a6672de1c66a881240
Fredrik Fordelsen <fredrikf2000@gmail.com>
2025-02-17 10:34:40 +0100

    reinstalled axios in frontend, and started working on employeelist component

---
+++ frontend/src/app/components/EmployeeList.js
@@ -0,0 +1,10 @@
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+
+const EmployeeList = () => {
+  return (
+    <div>EmployeeList</div>
+  )
+}
+
+export default EmployeeList
