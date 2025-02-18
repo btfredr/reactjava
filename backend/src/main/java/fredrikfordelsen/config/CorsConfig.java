0c54489d37a701f0f837a65505e344fb29023596
Fredrik Fordelsen <fredrikf2000@gmail.com>
2025-02-18 10:53:24 +0100

    changed SET to post

--- backend/src/main/java/fredrikfordelsen/config/CorsConfig.java
+++ backend/src/main/java/fredrikfordelsen/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of("http://localhost:3000")); // Allow Next frontend on port 3000
-        config.setAllowedMethods(List.of("GET", "SET", "PUT", "DELETE")); // Which HTTP methods that are allowed
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE")); // Which HTTP methods that are allowed
         config.setAllowedHeaders(List.of("*")); // Allowing all headers
         config.setAllowCredentials(true); // Allowing use of cookies and authentication
 
