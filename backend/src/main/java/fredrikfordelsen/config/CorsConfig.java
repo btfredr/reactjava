59509222c944c24335af092175e113cc8f9bd00b
Fredrik Fordelsen <fredrikf2000@gmail.com>
2025-02-18 10:47:14 +0100

    updated corsconfig with springboot 3 requirements

--- backend/src/main/java/fredrikfordelsen/config/CorsConfig.java
+++ backend/src/main/java/fredrikfordelsen/config/CorsConfig.java
@@ -1,17 +1,25 @@
 package fredrikfordelsen.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**") // Allows requests to all API endpoints
-        .allowedOrigins("http://localhost:3000") // Allow Next frontend on port 3000
-        .allowedMethods("GET", "SET", "PUT", "DELETE") // Which HTTP methods that are allowed
-        .allowedHeaders("*") // Allowing all headers
-        .allowedCredentials(true); // Allowing use of cookies and authentication
+public class CorsConfig {
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("http://localhost:3000")) // Allow Next frontend on port 3000
+        config.setAllowedMethods(List.of("GET", "SET", "PUT", "DELETE")) // Which HTTP methods that are allowed
+        config.setAllowedHeaders(List.of("*")) // Allowing all headers
+        config.setAllowCredentials(true); // Allowing use of cookies and authentication
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
     }
 }
