c69ee1362a10b79bdb8d855278334914910e16a7
Fredrik Fordelsen <fredrikf2000@gmail.com>
2025-02-24 19:43:59 +0100

    added states to contact page

--- frontend/src/app/kontakt/page.js
+++ frontend/src/app/kontakt/page.js
@@ -1,8 +1,32 @@
+"use client"
+
 import React from 'react'
+import Nav from '../components/Nav'
+import axios from 'axios';
 
 const Contact = () => {
+    // State for saving the user's form input
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        message: ""
+    });
+
+    // State for showing status messages to user
+    const [status, setStatus] = useState("");
+
+    // Handling change in input fields
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value});
+    }
   return (
-    <div>page</div>
+    <>
+        <Nav />
+
+        <div className="container">
+
+        </div>
+    </>
   )
 }
 
