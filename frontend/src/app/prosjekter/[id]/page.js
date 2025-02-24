1813f0fcc3377b1ad15bfc22f07b8751be37d848
Fredrik Fordelsen <fredrikf2000@gmail.com>
2025-02-24 19:05:03 +0100

    made dynamic project page

--- frontend/src/app/prosjekter/[id]/page.js
+++ frontend/src/app/prosjekter/[id]/page.js
@@ -3,11 +3,24 @@ import {useParams} from 'next/navigation'
 import axios from 'axios'
 
 const ProjectPage = () => {
-    
+    const {id} = useParams(); // Getting project ID from URL
+    const [project, setProject] = useState(null);
+
+    useEffect(() => {
+        axios
+            .get(`http:localhost:8081/api/projects/${id}`) // Adding URL to API URL
+            .then((response) => setProject(response.data))
+            .catch((error) => console.log("Feil ved henting av prosjekt", error));
+    }, [id]);
+
+    if (!project) return <h1>Laster prosjekt...</h1>; // Displaying message to user to show progress
+
   return (
     <div className="project-details">
-
-
+        <Image src={project.image} />
+        <h1>{project.title}</h1>
+        <p>{project.description}</p>
+        <p>Lokasjon: {project.location}</p>
     </div>
   )
 }
